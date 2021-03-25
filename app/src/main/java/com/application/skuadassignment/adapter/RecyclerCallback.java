aff763514d913aeb289d099f41e2fa979763c2cd
Deepak Kumar (Laptop) <deepak.kumar@netsetsoftware.com>
2021-03-25 22:46:40 +0530

    empty

--- app/src/main/java/com/application/skuadassignment/adapter/RecyclerCallback.java
+++
@@ -1,9 +0,0 @@
-package com.application.skuadassignment.adapter;
-
-import android.view.View;
-
-import androidx.databinding.ViewDataBinding;
-
-public interface RecyclerCallback<VM extends ViewDataBinding, T> {
-    void bindData(VM binder, T model, final int position, View itemView);
-}
